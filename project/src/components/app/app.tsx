b1526edd499eae4da646d5dc9df4003ee6c055a5
Natalya <m_d.r.lecter@protonmail.com>
2023-08-04 21:59:58 +0400

    routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,8 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
+import { AppRoute } from '../../const';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
 
 type AppMainProps = {
   proposalCount: number
@@ -6,7 +10,23 @@ type AppMainProps = {
 
 function App({proposalCount}: AppMainProps): JSX.Element {
   return (
-    <MainScreen proposalCount={proposalCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen proposalCount={proposalCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
